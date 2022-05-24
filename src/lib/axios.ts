a14ea15c2936ad726239d4763f365d4809cadae6
AhasEko <ahaseko1@gmail.com>
2022-05-24 23:10:38 +0700

    fix: setup gloval axios config

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,4 +1,8 @@
 import Axios from 'axios'
+import { getCookies } from './cookie';
+
+Axios.defaults.baseURL = 'https://api.example.com';
+Axios.defaults.headers.common['Authorization'] = getCookies('auth-token');
 
 const get = async (url: string) => {
 	
@@ -23,4 +27,13 @@ const post = async (url: any, data: any) => {
 	return resApi;
 }
 
-export { get, post };
+const GET = async (url: string) => {
+	return await Axios.get(process.env.REACT_APP_ENP_BE + url)
+}
+
+
+const POST = async (url: string, data: any) => {
+	return await Axios.post(process.env.REACT_APP_ENP_BE + url, data)
+}
+
+export { get, post, GET, POST };
